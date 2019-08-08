fc904aebef6da749d3378b62eb922d810c0da6d9
Jason012 <longge_li0123@163.com>
2019-08-08 16:36:33 +0800

    init springBoot

---
+++ miaosha/src/main/java/com/maioshaproject/App.java
@@ -0,0 +1,17 @@
+package com.maioshaproject;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
